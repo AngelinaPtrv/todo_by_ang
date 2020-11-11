3e0109b382bce2f9186656931edd553e38c02383
Angelina <angelshakirova@gmail.com>
2020-11-11 15:11:45 +0300

    request

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,13 +1,81 @@
-import React from "react";
+import React, {Component} from "react";
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from "./About.module.css";
 
-const About = () => {
-  return (
-    <div className={styles.wrap}>
-      <h1>About</h1>
-    </div>
-  )
-}
+const name = 'kjghfjg';
+const URL = 'https://api.github.com/users/' + name;
 
-export default About;
+export default class About extends Component {
+  state = {
+    name: '',
+    bio: '',
+    email: '',
+    isLoading: true,
+    repoUrl: '',
+    repoList: [],
+    fetchRequest: true,
+    loadFailure: false
+  }
+
+  componentDidMount() {
+    let func = async () => {
+      await fetch(URL)
+      .then(resolve => resolve.ok
+        ? resolve.json()
+        : Promise.reject())
+      .then((json) => {
+        this.setState({
+          name: json.name,
+          bio: json.bio,
+          email: json.email,
+          repoUrl: json.repos_url,
+          fetchRequest: false
+        })
+    });
+
+      await fetch(this.state.repoUrl)
+      .then(resolve => resolve.json())
+      .then((json) => {
+        let arrName = json.map(elem => elem.name);
+        this.setState({
+          repoList: arrName,
+          isLoading: false
+        })
+      })
+      };
+    func().catch(() => this.setState({loadFailure: true, isLoading: false}));
+  }
+
+  render() {
+    const link = `https://github.com/`;
+    const {isLoading, repoList, name, bio, email, fetchRequest, loadFailure} = this.state;
+    return (
+      <div className={styles.wrap}>
+        <h1>{isLoading ? <CircularProgress/> : 'About me'}</h1>
+        <div>
+          {!fetchRequest ?
+            <ul>
+              {name && <li>{name}</li>}
+              {bio && <li>{bio}</li>}
+              {email && <li>{email}</li>}
+            </ul> :
+            'User not found'}
+        </div>
+        {(!isLoading && !loadFailure)
+          ? <div>
+              <h1>My repos</h1>
+                <ol>
+                  {repoList.map((repo, index) => (
+                  <li key={index}>
+                    <a href={`${link}${name}/${repo}`} className={styles.link}>{repo}</a>
+                  </li>
+                  ))}
+                </ol>
+            </div>
+          : <div>Repos no found</div>
+        }
+      </div>
+    )
+  }
+}
