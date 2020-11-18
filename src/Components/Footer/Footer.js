a3d0886c805e2df456f3a5b6306536cf60fcc7dc
Angelina <angelshakirova@gmail.com>
2020-11-18 14:04:29 +0300

    filters in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import Button from "@material-ui/core/Button";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import PropTypes from 'prop-types';
 
-const Footer = ({count}) => {
+const Footer = ({count, toShowAll, toShowActive, toShowCompleted, toClearCompleted}) => {
 
   Footer.prototype = {
     count: PropTypes.number.isRequired
@@ -17,11 +17,25 @@ const Footer = ({count}) => {
         size="small"
         variant="text"
         aria-label="text outlined button group">
-        <Button style={{fontSize: '9px', color: '#c4c4c4'}}>All</Button>
-        <Button style={{fontSize: '9px', color: '#c4c4c4'}}>Active</Button>
-        <Button style={{fontSize: '9px', color: '#c4c4c4'}}>Completed</Button>
+        <Button style={{fontSize: '9px', color: '#c4c4c4'}}
+                onClick={() => toShowAll()}
+        >
+          All
+        </Button>
+        <Button style={{fontSize: '9px', color: '#c4c4c4'}}
+          onClick={() => toShowActive()}
+        >
+          Active
+        </Button>
+        <Button style={{fontSize: '9px', color: '#c4c4c4'}}
+          onClick = {() => toShowCompleted()}
+        >
+          Completed
+        </Button>
       </ButtonGroup>
-      <Button style={{fontSize: '9px', color: '#c4c4c4'}}>Clear completed</Button>
+      <Button style={{fontSize: '9px', color: '#c4c4c4'}}
+              onClick={() => toClearCompleted()}
+      >Clear completed</Button>
     </div>
   )
 
--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -4,33 +4,29 @@ import styles from "./ItemList.module.css";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
-export default class ItemList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+class ItemList extends Component {
   onChangeValue = (id, value) => {
-    console.log("id", id);
-    console.log("value", value);
     this.props.onDoubleChange(id, value);
   }
 
   render() {
     let element = this.props.todoItems.map(item => {
-      const {id, showIcon,  ...itemProps} = item;
+      const {id, showIcon,  visible, ...itemProps} = item;
       return (
         <div key={id}
              onMouseOver={()=> this.props.itemHover(id)}
              onMouseOut={() => this.props.itemHover(id)}
-             className={styles.wrap}>
+             className={visible ? styles.wrap : styles.invisible}
+        >
           <Item
             id={id}
             value={itemProps.value}
             isDone={itemProps.isDone}
+            showIcon={showIcon}
             onClickDone={this.props.onClickDone}
             setFieldValue={this.onChangeValue}
           />
-          <div className={showIcon ? styles.delete : styles.nodelete}>
+          <div className={showIcon ? styles.visible : styles.invisible}>
             <IconButton
               onClick={()=>this.props.onClickDelete(id)}>
               <DeleteOutlinedIcon/>
@@ -47,50 +43,4 @@ export default class ItemList extends Component {
   }
 }
 
-// const ItemList = ({todoItems, itemHover, onClickDone, onClickDelete, onDoubleClick}) => {
-//
-//   const onChangeValue = (e) => {
-//     this.setState({
-//     value: e.target.value
-//     })
-//   }
-//
-//   let element = todoItems.map(item => {
-//     const {id, showIcon,  ...itemProps} = item;
-//     return (
-//       <div key={id}
-//            onMouseOver={()=> itemHover(id)}
-//            onMouseOut={() => itemHover(id)}
-//            className={styles.wrap}>
-//         <Item
-//           id={id}
-//           value={itemProps.value}
-//           isDone={itemProps.isDone}
-//           onClickDone={onClickDone}
-//           onDoubleClick={() => onDoubleClick(id)}
-//         />
-//         <div className={showIcon ? styles.delete : styles.nodelete}>
-//           <IconButton
-//             onClick={()=>onClickDelete(id)}>
-//             <DeleteOutlinedIcon/>
-//           </IconButton>
-//         </div>
-//       </div>
-//     )
-//   });
-//
-//   ItemList.prototype = {
-//     todoItems: PropTypes.object.isRequired,
-//     itemHover: PropTypes.func.isRequired,
-//     onClickDone: PropTypes.func.isRequired,
-//     onClickDelete: PropTypes.func.isRequired
-//   }
-//
-//   return (
-//   <ul className={styles.list}>
-//     {element}
-//   </ul>
-//   )
-// };
-//
-// export default ItemList;
+export default ItemList;
